721b54769e5a1c6cf7e3ce2434b55dbcdc0892d9
bconti123 <bryantcconti@gmail.com>
2024-09-04 18:14:32 -0700

    Create app, server, and errors

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+"use strict";
+const express = require("express");
+
+const { NotFoundError } = require("./expressError");
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    return next(new NotFoundError());
+});
+
+app.use((err, req, res, next) => {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+  
+    return res.status(status).json({
+      error: { message, status },
+    });
+});
+
+module.exports = app;
