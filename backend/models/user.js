aa892d41bd432e5f654ec8452ac23209d5ce6016
bconti123 <bryantcconti@gmail.com>
2024-10-02 21:25:28 -0700

    Create models

---
+++ backend/models/user.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const { sqlForPartialUpdate } = require("../helper/sql");
+const {
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+} = require("../expressError");
+
+const { BCRYPT_WORK_FACTOR } = require("../config");
+
+class User {
+  /** authenticate user with username, password.
+   *
+   * Returns { username, first_name, last_name, email, is_admin }
+   *
+   * Throws UnauthorizedError is user not found or wrong password.
+   **/
+
+  static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT username,
+                  password,
+                  first_name AS "firstName",
+                  last_name AS "lastName",
+                  email,
+                  is_admin AS "isAdmin"
+           FROM users
+           WHERE username = $1`,
+      [username]
+    );
+
+    const user = result.rows[0];
+
+    if (user) {
+      const isValid = await bcrypt.compare(password, user.password);
+      if (isValid === true) {
+        delete user.password;
+        return user;
+      }
+    }
+
+    throw new UnauthorizedError("Invalid username/password");
+  }
+
+  /** Register user with data.
+   *
+   * Returns { username, firstName, lastName, email, isAdmin }
+   *
+   * Throws BadRequestError on duplicates.
+   **/
+
+  static async register({
+    username,
+    password,
+    firstName,
+    lastName,
+    email,
+    isAdmin,
+  }) {
+    const duplicateCheck = await db.query(
+      `SELECT username
+           FROM users
+           WHERE username = $1`,
+      [username]
+    );
+  }
+}
