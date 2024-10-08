f30f9ad1d7be657b2d4dee5df775a9f1de5a2014
bconti123 <bryantcconti@gmail.com>
2024-10-07 20:25:13 -0700

    Fixed schema

--- backend/Database/schema.sql
+++ backend/Database/schema.sql
@@ -18,44 +18,44 @@ CREATE TABLE roles (
     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE permissions (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR(255) UNIQUE NOT NULL,
-    "description" VARCHAR(255),
-    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE role_permissions (
-    "id" SERIAL PRIMARY KEY,
-    "role_id" INTEGER NOT NULL REFERENCES roles(id),
-    "permission_id" INTEGER NOT NULL REFERENCES permissions(id),
-    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
-
 CREATE TABLE user_roles (
     "id" SERIAL PRIMARY KEY,
-    "user_id" INTEGER NOT NULL REFERENCES users(id),
-    "role_id" INTEGER NOT NULL REFERENCES roles(id),
+    "user_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    "role_id" INTEGER REFERENCES roles(id) ON DELETE CASCADE,
     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE activity_logs (
-    "id" SERIAL PRIMARY KEY,
-    "user_id" INTEGER NOT NULL REFERENCES users(id),
-    "action" VARCHAR(255) NOT NULL,
-    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE permissions (
+--     "id" SERIAL PRIMARY KEY,
+--     "name" VARCHAR(255) UNIQUE NOT NULL,
+--     "description" VARCHAR(255),
+--     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+-- );
 
-CREATE TABLE visitors (
-    "id" SERIAL PRIMARY KEY,
-    "user_id" INTEGER NOT NULL REFERENCES users(id),
-    "ip" VARCHAR(255) NOT NULL,
-    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE role_permissions (
+--     "id" SERIAL PRIMARY KEY,
+--     "role_id" INTEGER NOT NULL REFERENCES roles(id),
+--     "permission_id" INTEGER NOT NULL REFERENCES permissions(id),
+--     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+-- );
 
-CREATE TABLE sessions (
-    "id" SERIAL PRIMARY KEY,
-    "user_id" INTEGER NOT NULL REFERENCES users(id),
-    "token" VARCHAR(255) NOT NULL,
-    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE activity_logs (
+--     "id" SERIAL PRIMARY KEY,
+--     "user_id" INTEGER NOT NULL REFERENCES users(id),
+--     "action" VARCHAR(255) NOT NULL,
+--     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- CREATE TABLE visitors (
+--     "id" SERIAL PRIMARY KEY,
+--     "user_id" INTEGER NOT NULL REFERENCES users(id),
+--     "ip" VARCHAR(255) NOT NULL,
+--     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- CREATE TABLE sessions (
+--     "id" SERIAL PRIMARY KEY,
+--     "user_id" INTEGER NOT NULL REFERENCES users(id),
+--     "token" VARCHAR(255) NOT NULL,
+--     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+-- );
