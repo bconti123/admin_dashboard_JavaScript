b73d71cd069f2be2932a46f5006769f0871df7b3
bconti123 <bryantcconti@gmail.com>
2024-10-07 20:32:33 -0700

    Fixed schema

--- backend/Database/schema.sql
+++ backend/Database/schema.sql
@@ -3,6 +3,8 @@ CREATE TABLE users (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR(255) UNIQUE NOT NULL,
     "password" VARCHAR(255) NOT NULL,
+    "first_name" VARCHAR(255),
+    "last_name" VARCHAR(255),
     "email" VARCHAR(255) UNIQUE NOT NULL,
         CHECK (position('@' IN "email") > 1),
     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
