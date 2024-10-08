c04c5b5ba293cae831b38cb8b41f61dca9c379ab
bconti123 <bryantcconti@gmail.com>
2024-10-07 20:25:19 -0700

    Fixed seed

--- backend/Database/seed.sql
+++ backend/Database/seed.sql
@@ -1,5 +1,17 @@
-INSERT INTO users (username, password, email, role) 
+INSERT INTO users (username, password, email) 
 VALUES
-('Root', 'rootpass', 'admin@localhost.com', 'admin'),
-('Admin', 'adminpass', 'admin@localhost.com', 'admin'),
-('User', 'userpass', 'user@localhost.com', 'user');
+('Root', 'rootpass', 'root@localhost.com'),
+('Admin', 'adminpass', 'admin@localhost.com'),
+('User', 'userpass', 'user@localhost.com');
+
+INSERT INTO roles (name, description) 
+VALUES
+('root', 'root role'),
+('admin', 'admin role'),
+('user', 'user role');
+
+INSERT INTO user_roles (user_id, role_id) 
+VALUES
+(1, 1),
+(2, 2),
+(3, 3);
