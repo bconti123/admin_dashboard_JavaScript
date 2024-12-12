5a9f13e49754dc775d8a974940b368ef1d0ba448
bconti123 <bryantcconti@gmail.com>
2024-12-12 10:23:23 -0800

    Auth Test

--- backend/Tests/routes/auth.test.js
+++ backend/Tests/routes/auth.test.js
@@ -2,7 +2,7 @@
 
 const request = require("supertest");
 const app = require("../../app");
-const db = require("../../db");
+
 const {
     commonBeforeAll,
     commonBeforeEach,
@@ -28,5 +28,56 @@ describe("POST /auth/token", () => {
         expect(response.body).toEqual({
             token: expect.any(String),
         });
+    });
+
+    test("unauth with bad credentials", async () => {
+        const response = await request(app)
+            .post("/auth/token")
+            .send({
+                username: "Root",
+                password: "wrong",
+            });
+        expect(response.statusCode).toEqual(401);
+    });
+
+    test("bad request with schema", async () => {
+        const response = await request(app)
+            .post("/auth/token")
+            .send({
+                username: "Root",
+            });
+        expect(response.statusCode).toEqual(400);
+        expect(response.error.message).toEqual(expect.any(String));
+    });
+});
+
+// Registration tests
+describe("POST /auth/register", () => {
+    test("works", async () => {
+        const response = await request(app)
+            .post("/auth/register")
+            .send({
+                username: "NewUser",
+                password: "newpassword",
+                firstName: "new",
+                lastName: "user",
+                email: "new@localhost.com",
+            });
+        expect(response.body).toEqual({
+            token: expect.any(String),
+        });
+    });
+
+    test("bad request with schema", async () => {
+        const response = await request(app)
+            .post("/auth/register")
+            .send({
+                username: "NewUser",
+                password: "newpassword",
+                firstName: "new",
+                lastName: "user",
+            });
+        expect(response.statusCode).toEqual(400);
+        expect(response.error.message).toEqual(expect.any(String));
     });
 });
